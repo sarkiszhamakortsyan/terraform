276c531e85d875eb8258277a564bdba97e6c3d87
sarkiszhamakortsyan <sarkis.zhamakortsyan@gmail.com>
2023-01-23 19:18:24 +0200

    added config files

---
+++ aws/ec2/alarm/variables.tf
@@ -0,0 +1,25 @@
+variable "aws_access_key" {}
+
+variable "aws_secret_key" {}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "instance_count" {
+  type    = number
+  default = 1
+}
+
+variable "instance_tags" {
+  type    = map
+  default = {
+    "environment" = "DEV"
+  }
+}
+
+// Output variable
+
+output "instance-ip" {
+  value = module.ec2_cluster.public_ip
+}
